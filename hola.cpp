caa9d21ab915cc59055e7dd401a727285e97de38
Diego Josuan Ornelas Durán <150072192+diego-ornelas4952@users.noreply.github.com>
2024-01-31 16:26:43 -0600

    Nueva prueba

--- hola.cpp
+++ hola.cpp
@@ -6,6 +6,7 @@ int main(){
 cout>>"Hola Mundo">>endl;
 cout>>"Este es un cambio online">>endl;
 cout>>"Y este otro es un cambio en la terminal de macOS">>endl;
+cout>>"Ahora a probar con este">>endl;
 
 return 0;
 
