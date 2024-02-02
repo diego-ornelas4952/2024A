8894eea747a5112520cbb30b11ac052262f28401
Diego Josuan Ornelas Durán <150072192+diego-ornelas4952@users.noreply.github.com>
2024-02-01 21:48:30 -0600

    Nuevo archivo

---
+++ DatosPersonales_OrnelasDurán.cpp
@@ -0,0 +1,21 @@
+/***************PRESENTACIÓN***************
+Nombre: Diego Josuan Ornelas Durán
+Fecha: 30 de Enero de 2024
+Programa: datos_personales.cpp
+Centro Universitario de Los Altos / Universidad de Guadalajara
+Ingeniería en Computación / Segundo Semestre
+Profesor: Sergio Franco Casillas
+Descripción: Mostrar datos personales del alumno
+******************************************/
+
+#include <cstdio>
+int main(){
+    printf("Mi nombre es: Diego Josuan Ornelas Durán. \n");
+    printf("Actualmente tengo 18 años. \n");
+    printf("Soy de Jalostotitlán, Jalisco, México. \n");
+    printf("El día de hoy es Martes 30 de Enero de 2024. \n");
+    printf("Estoy estudiando la carrera de Ingeniería en Computación. \n");
+    printf("Actualmente estoy cursando el segundo semestre de la carrera. \n");
+
+    return 0;
+}
